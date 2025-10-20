064b6c75994a73d79b8ef4bf7a317d783deb47b9
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-10-20 13:46:34 +0100

    expanded test coverage

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,3 +16,10 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
+
+
+def test_invalid_input():
+    """
+    Test the function `minimum` in functionality.py
+    """
+    assert ci_course.minimum('not valid input') == None
